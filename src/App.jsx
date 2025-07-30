79c8ea3a590853bb00e018900a52a9ec23bbede0
rohitsinghwho <rohiisingh8655@gmail.com>
2025-07-30 14:58:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [name,setName]=useState({
+    firstName:"",
+    lastName:""
+  });
+  const [showName,setShowName]=useState(false)
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setShowName(!showName)
+  }
+  return (
+    <div
+    style={{
+      display:"flex",
+      gap:"20px",
+        flexDirection:"column"
+
+    }}
+    >
+    <form onSubmit={handleSubmit}>
+      <div
+      style={{
+        display:"flex",
+        gap:"5px",
+      }}
+      >
+      <label htmlFor="firstName">First Name:</label>
+      <input required type="text" value={name.firstName} onChange={(e)=>setName({firstName:e.target.value})}/>
+      </div>
+      <div
+      style={{
+        display:"flex",
+        gap:"5px"
+      }}
+      >
+      <label htmlFor="LastName">Last Name:</label>
+      <input required type="text" value={name.lastName} onChange={(e)=>setName({lastName:e.target.value})}/>
+      </div>
+      <button>Submit</button>
+    </form>
+
+    {
+      showName&&
+      <span>Full Name: {name.firstName} {name.lastName}</span>
+    }
+
+    </div>
+  )
+}
+
+export default App
